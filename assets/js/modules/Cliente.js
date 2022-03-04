1feed1542edce8633326ffadbc214df9590802b2
Joshua Espinoza <josh6669@gmail.com>
2022-03-04 20:42:38 -0300

    Done without babel

---
+++ assets/js/modules/Cliente.js
@@ -0,0 +1,20 @@
+import Impuestos from "./Impuestos.js";
+export default class Cliente {
+  constructor(nombre, impuesto) {
+    this._nombre = () => nombre;
+    this._impuesto = () => impuesto;
+  }
+
+  get nombre() {
+    return this._nombre();
+  }
+  set nombre(nombre) {
+    this._nombre = () => nombre;
+  }
+
+  calcularImpuesto() {
+    return (
+      (this._impuesto().montoBrutoAnual - this._impuesto().deducciones) * 21/100
+    );
+  }
+}
